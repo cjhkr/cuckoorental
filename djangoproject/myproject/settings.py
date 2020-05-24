7d9dce8341b187c9867617a6e97b2d5bb7236937
최재혁 <wogur247@hanyang.ac.kr>
2020-05-25 02:14:04 +0900

    2

--- djangoproject/myproject/settings.py
+++ djangoproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '48v!!ef77dfjk^)_rv%8qi9a4*2l6e-ad(+bcvwa^^o#tket_w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-env2.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
